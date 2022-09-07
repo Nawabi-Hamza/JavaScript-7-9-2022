82e5c6870c1e838328c5224c7aa9db34ed2671f5
Hamza-Nawabi <h.nawabi007@gmail.com>
2022-09-07 12:05:08 +0430

    some worked

--- js3.js
+++ js3.js
@@ -6,7 +6,19 @@ function myS(){
 
    count ++
    console.log(count)
-   ea.push(count)
-   console.log('total array : '+ea)
+   if (count % 3 == 0 && count % 5 == 0){
+    ea.push(" Iam Fine ")
+    console.log('array : '+ea)
+   }
+
+   else if (count%3==0){
+    ea.push("Hi")
+    console.log(' array : '+ea)
+
+   }
+   else if (count%5==0){
+    ea.push(" How are you ")
+    console.log(' array : '+ea)
+   }
    
 }
